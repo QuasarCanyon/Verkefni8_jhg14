e1caf145130a70039372757040ac298676b795e2
QuasarCanyon <jongudm@gmail.com>
2019-11-06 00:09:56 +0000

    Byrjun á event handlers

--- scripts.js
+++ scripts.js
@@ -13,22 +13,40 @@ const text = (() => {
   function init(_form, _items) {
     items = _items;
     _form.addEventListener('submit', formHandler);
-
+    
     // TODO láta hluti í _items virka
+    const itemTick = items.querySelectorAll('.item__checkbox');
+    const itemButton = items.querySelectorAll('.item__button');
+    const itemText = items.querySelectorAll('.item__text');
+
+    for (let i = 0; i < itemTick.length; i++) {
+      itemTick[i].addEventListener('change', finish);
+    }
+
+    for (let i = 0; i < itemButton.length; i++) {
+      itemButton[i].addEventListener('push', deleteItem);
+    }
+
+    for (let i = 0; i < itemText.length; i++) {
+      itemText[i].addEventListener('click', edit);
+    }
   }
 
   function formHandler(e) {
     e.preventDefault();
-
+    
     console.log('halló heimur');
   }
 
   // event handler fyrir það að klára færslu
   function finish(e) {
+    const tick = e.srcElement.parentElement;
+    tick.classList.toggle('item--done');
   }
 
   // event handler fyrir það að breyta færslu
   function edit(e) {
+    
   }
 
   // event handler fyrir það að klára að breyta færslu
